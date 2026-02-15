9456a41c617bbb9e2bd65b4f4aa5473eb2ad365a
notEvanN <vudutminh@gmail.com>
2026-02-15 19:11:40 +1100

    11: widget screen router

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,18 +1,30 @@
 "use client";
 
-// import { WidgetFooter } from "../components/widget-footer";
-// import { WidgetHeader } from "../components/widget-header";
-import { WidgetAuthScreen } from "../screens/widget-auth-screen";
+import { useAtomValue } from "jotai";
+import { WidgetAuthScreen } from "@/modules/widget/ui/screens/widget-auth-screen";
+import { screenAtom } from "@/modules/widget/atoms/widget-atoms";
 
 interface Probs {
     organizationId: string;
 };
 
 export const WidgetView = ({ organizationId }: Probs) => {
+    const screen = useAtomValue(screenAtom);
+
+    const screenComponents = {
+        error: <p>Something went wrong</p>,
+        loading: <p>Loading...</p>,
+        auth: <WidgetAuthScreen />,
+        voice: <p>Voice Screen</p>,
+        inbox: <p>Inbox Screen</p>,
+        selection: <p>Selection Screen</p>,
+        chat: <p>Chat Screen</p>,
+        contact: <p>Contact Screen</p>,
+    }
+
     return (
         <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-            <WidgetAuthScreen />
-            {/* <WidgetFooter /> */}
+            {screenComponents[screen]}
         </main>
     );
 };
