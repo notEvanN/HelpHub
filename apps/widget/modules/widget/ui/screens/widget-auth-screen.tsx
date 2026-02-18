b7991b20b44879c6dddb38681e2d750c9a84ccd7
notEvanN <vudutminh@gmail.com>
2026-02-18 21:24:31 +1100

    12.0: widget loading

--- apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
@@ -1,4 +1,4 @@
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
@@ -16,6 +16,8 @@ import { api } from "@workspace/backend/_generated/api";
 import { userAgent } from "next/server";
 import { platform } from "os";
 import { Doc } from "@workspace/backend/_generated/dataModel";
+import { useAtomValue, useSetAtom } from "jotai";
+import { contactSessionIdAtomFamily, organizationIdAtom } from "../../atoms/widget-atoms";
 
 const formSchema = z.object({
     name: z.string().min(1, "Name is required"),
@@ -25,6 +27,11 @@ const formSchema = z.object({
 const organizationId = "example-org-id";
 
 export const WidgetAuthScreen = () => {
+    const organizationId = useAtomValue(organizationIdAtom);
+    const setContactSessionId = useSetAtom(
+        contactSessionIdAtomFamily(organizationId || "")
+    );
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -60,7 +67,7 @@ export const WidgetAuthScreen = () => {
             metadata,
         });
 
-        console.log({ contactSessionsId});
+        setContactSessionId(contactSessionsId);
     };
 
     return (
