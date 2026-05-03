f67cca02d9fe57fddc82981aa942395d982d5d8c
notEvanN <vudutminh@gmail.com>
2026-05-03 20:41:12 +1000

    16.0: widget inbox

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -7,6 +7,7 @@ import { WidgetErrorScreen } from "@/modules/widget/ui/screens/widget-error-scre
 import { WidgetLoadingScreen } from "@/modules/widget/ui/screens/widget-loading-screen";
 import { WidgetSelectionScreen } from "@/modules/widget/ui/screens/widget-selection-screen";
 import { WidgetChatScreen } from "@/modules/widget/ui//screens/widget-chat-screen";
+import { WidgetInboxScreen } from "@/modules/widget/ui/screens/widget-inbox-screen";
 
 interface Props {
     organizationId: string | null;
@@ -20,7 +21,7 @@ export const WidgetView = ({ organizationId }: Props) => {
         error: <WidgetErrorScreen />,
         auth: <WidgetAuthScreen />,
         voice: <p>Voice Screen</p>,
-        inbox: <p>Inbox Screen</p>,
+        inbox: <WidgetInboxScreen />,
         selection: <WidgetSelectionScreen />,
         chat: <WidgetChatScreen />,
         contact: <p>Contact Screen</p>,
